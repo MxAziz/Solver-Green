f1d80622534a209264138e832ee414c43cb7a280
MxAziz <hereabdulaziz@gmail.com>
2026-08-01 22:17:33 +0600

    remove some unused code and modify dashboad layout style

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,10 @@
+import Sidebar from "@/components/dashboard/common/Sidebar";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex">
-      {/* <Sidebar /> */}
-      <main className="flex-1 p-6">{children}</main>
+    <div className="flex min-h-screen bg-[#F4F7F3]">
+      <Sidebar />
+      <main className="flex-1 p-4 sm:p-6 lg:p-8">{children}</main>
     </div>
   );
 }
